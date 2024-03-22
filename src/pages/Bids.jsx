1f74cf14a502f789679e5da4cc4a2cfa8328f9a6
dalienst <oduordalienst@gmail.com>
2024-03-22 08:54:55 +0300

    add coloir

--- src/pages/Bids.jsx
+++ src/pages/Bids.jsx
@@ -62,78 +62,80 @@ function Bids() {
 
   return (
     <>
-      <div className="container">
-        <section>
-          <h5 className="text-secondary text-uppercase mb-2">
-            <span>
-              <Link to={links.Dashboard} className="btn">
-                <i className="bi bi-arrow-left"></i>
-              </Link>
-            </span>
-            Bids
-          </h5>
-          <h3 className="fw-bold">Your Bids</h3>
-          <p className="fst-italic">Bids Placed: {bids?.count}</p>
-          <hr />
-          {bids?.results?.map((bid) => (
-            <>
-              <div className="mb-3">
-                <article>
-                  <h6 className="font-monospace">Bid Proposal - {bid?.id}</h6>
-                  <p className="fst-italic">Status: {bid?.status}</p>
+      <div className="container-fluid page">
+        <div className="container">
+          <section>
+            <h5 className="text-secondary text-uppercase mb-2">
+              <span>
+                <Link to={links.Dashboard} className="btn">
+                  <i className="bi bi-arrow-left"></i>
+                </Link>
+              </span>
+              Bids
+            </h5>
+            <h3 className="fw-bold">Your Bids</h3>
+            <p className="fst-italic">Bids Placed: {bids?.count}</p>
+            <hr />
+            {bids?.results?.map((bid) => (
+              <>
+                <div className="mb-3">
+                  <article>
+                    <h6 className="font-monospace">Bid Proposal - {bid?.id}</h6>
+                    <p className="fst-italic">Status: {bid?.status}</p>
 
-                  <p>{bid?.proposal}</p>
-                  <div>
-                    <Button
-                      variant="outline-primary btn-sm"
-                      onClick={() => fetchProjectBid(bid?.project)}
-                    >
-                      Project Detail
-                    </Button>
-                  </div>
-                </article>
-                <hr />
-              </div>
-            </>
-          ))}
+                    <p>{bid?.proposal}</p>
+                    <div>
+                      <Button
+                        variant="outline-primary btn-sm"
+                        onClick={() => fetchProjectBid(bid?.project)}
+                      >
+                        Project Detail
+                      </Button>
+                    </div>
+                  </article>
+                  <hr />
+                </div>
+              </>
+            ))}
 
-          <Modal show={show} onHide={handleClose}>
-            <div className="modal-header">
-              <h5 className="modal-title">{bidProjects?.name}</h5>
-              <button
-                type="button"
-                className="btn-close"
-                onClick={handleClose}
-              ></button>
-            </div>
-            <div className="modal-body">
-              <p className="fst-italic">
-                Ksh {bidProjects?.min_price} - Ksh {bidProjects?.max_price}
-              </p>
-              <p>{bidProjects?.description}</p>
-              <div className="btn-group" role="group">
-                <button className="btn btn-sm btn-outline-dark">
-                  {bidProjects?.project_category}
-                </button>
-                <button className="btn btn-sm btn-outline-dark">
-                  {bidProjects?.project_type}
-                </button>
-                <button className="btn btn-sm btn-outline-dark">
-                  {bidProjects?.project_duration}
+            <Modal show={show} onHide={handleClose}>
+              <div className="modal-header">
+                <h5 className="modal-title">{bidProjects?.name}</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={handleClose}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <p className="fst-italic">
+                  Ksh {bidProjects?.min_price} - Ksh {bidProjects?.max_price}
+                </p>
+                <p>{bidProjects?.description}</p>
+                <div className="btn-group" role="group">
+                  <button className="btn btn-sm btn-outline-dark">
+                    {bidProjects?.project_category}
+                  </button>
+                  <button className="btn btn-sm btn-outline-dark">
+                    {bidProjects?.project_type}
+                  </button>
+                  <button className="btn btn-sm btn-outline-dark">
+                    {bidProjects?.project_duration}
+                  </button>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={handleClose}
+                >
+                  Close
                 </button>
               </div>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                onClick={handleClose}
-              >
-                Close
-              </button>
-            </div>
-          </Modal>
-        </section>
+            </Modal>
+          </section>
+        </div>
       </div>
     </>
   );
